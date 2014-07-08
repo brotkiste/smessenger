2250336c0f0d74e304acb1ff8f2f1776aceb4a91
brotkiste <mkraus4000@web.de>
2014-07-08 18:16:57 +0200

    OPEN - task 1: C_Network.java implementieren http://github.com/brotkiste/smessenger/issues/issue/1

--- client/C_Network.java
+++ client/C_Network.java
@@ -1,22 +1,35 @@
 package client;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-
+/**
+ * Stellt einen Socket zur Verfügung
+ * 
+ * @author Matthias Kraus
+ *
+ */
 public class C_Network {
 	Socket s;
 	BufferedWriter out;
+	BufferedReader in;
 	
+	/**
+	 * 
+	 * @param ip Zieladresse
+	 * @param port Zielport
+	 */
 	public C_Network(String ip, int port){
 		try {
 			s = new Socket(ip, port);
 		
 			out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-		
+			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			System.out.println(ip + " konnte nicht gefunden werden!");
@@ -27,12 +40,46 @@ public class C_Network {
 		}
 	}
 	
+	/**
+	 * Verschickt einen String
+	 * @param s Nachricht
+	 */
 	public void stringSenden(String s){
 		try {
 			out.write(s);
 		
 		out.newLine();
 		out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * Wartet auf einen String. Der String muss unbedingt mit einem Zeilenumbruch beendet werden!
+	 * @return empfangener String
+	 * TODO ungetestet!
+	 */
+	public String stringEmpfangen(){
+		try {
+			return in.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "FEHLER!! C_Network.stringEmpfangen()";
+	}
+	
+	/**
+	 * sollte aufgerufen werden, wenn die Verbindung nicht mehr benötigt wird
+	 */
+	public void socketSchliessen(){
+		try{
+			out.close();
+			in.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
