2197639eaa417a1aa191d47ea05a2f00c5cf8794
brotkiste <mkraus4000@web.de>
2014-07-08 17:29:28 +0200

    erzeugt ein Netzwerkobjekt, dass sich mit localhost:5555 verbindet

---
+++ client/C_Controller.java
@@ -0,0 +1,13 @@
+package client;
+
+public class C_Controller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		C_Network net = new C_Network("localhost", 5555);
+	}
+
+}
